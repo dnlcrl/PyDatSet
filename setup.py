91059b05f6dbda2bbe389e0d721db6b90dc6e285
dnlcrl <ciriello.daniele@gmail.com>
2016-04-17 17:07:57 +0200

    again trying to fix pip install

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
-#coding: utf-8
+# coding: utf-8
 
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
-	name = "pydatset",
-	author = "Daniele Ettore Ciriello",
-	author_email = "ciriello.daniele@gmail.com",
-	version = "0.1",
-	license = "MIT",
-	url = "https://github.com/dnlcrl/PyDatSet",
-	download_url = "https://github.com/dnlcrl/PyDatSet",
-	description = "Load and augment various datasets in Python for computer vision purposes",
-	py_modules = "",
-	packages = ['pydatset'],
-	install_requires = ['numpy', 'scipy', 'cv2', 'scikit_image'],
-	scripts = ""
+    name="pydatset",
+    author="Daniele Ettore Ciriello",
+    author_email="ciriello.daniele@gmail.com",
+    version="0.1",
+    license="MIT",
+    url="https://github.com/dnlcrl/PyDatSet",
+    download_url="https://github.com/dnlcrl/PyDatSet",
+    description="Load and augment various datasets in Python for computer vision purposes",
+    py_modules="",
+    packages=['pydatset'],
+    install_requires=required,
+    scripts=""
 )
